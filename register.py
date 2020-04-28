2bffe7b0f270c2b09df632946ee6c0605ba74cc1
Rasmus Munk <munk1@live.dk>
2020-04-28 15:00:01 +0200

    Rename main to start and add a template for the workflow

---
+++ register.py
@@ -0,0 +1,23 @@
+import oci
+
+if __name__ == "__main__":
+    # Spawn an oci instance
+    print("Hello")
+
+    # Register the live instance
+
+
+    # Schdule the container
+
+
+    # Pass job file
+
+
+    # Run simulation inside the container
+
+
+    # transfer back the results
+
+
+    # Spawn, schdule container to execute on instance
+    
