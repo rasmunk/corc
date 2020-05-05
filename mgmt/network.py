5e01a8ef699d11fda0e3d4fc598c4f51c34f57b4
Rasmus Munk <munk1@live.dk>
2020-05-05 18:19:59 +0200

    Update the network module to the new oci_helper interface, while add the ability to create and destroy a full vcn stack

--- mgmt/network.py
+++ mgmt/network.py
@@ -1,29 +1,98 @@
+import oci
 from oci.core import VirtualNetworkClient, VirtualNetworkClientCompositeOperations
 from oci.core.models import CreateVcnDetails, Vcn
+from oci.core.models import RouteTable, CreateRouteTableDetails, UpdateRouteTableDetails
+from oci.core.models import InternetGateway
+from oci.core.models import SecurityList
+from oci.core.models import DhcpOptions
+from oci.core.models import LocalPeeringGateway
+from oci.core.models import NatGateway
+from oci.core.models import ServiceGateway
+from oci.core.models import Subnet, CreateSubnetDetails
 from args import get_arguments, OCI, NETWORK
-from oci_helpers import new_client, create_vcn, delete_vcn, get_vcn, list_vcn
+from oci_helpers import (
+    new_client,
+    prepare_route_rule,
+    list_entities,
+    get,
+    create,
+    delete,
+    update,
+)
 
 
 def new_vcn_stack(
     network_client,
     compartment_id,
     name=None,
-    vcn_id=None,
-    cidr_block=None,
+    vcn_cidr_block=None,
+    subnet_cidr_block=None,
     **vcn_kwargs
 ):
-    if not cidr_block:
-        cidr_block = "10.0.0.0/16"
+    if not vcn_cidr_block:
+        vcn_cidr_block = "10.0.0.0/16"
 
     if name:
         vcn_kwargs.update({"display_name": name})
 
-    vcn = create_vcn(network_client, compartment_id, cidr_block, **vcn_kwargs)
+    stack = {}
+    create_vcn_details = CreateVcnDetails(
+        cidr_block=vcn_cidr_block, compartment_id=compartment_id, display_name=name
+    )
+    vcn = create(
+        network_client, "create_vcn", Vcn, create_vcn_details=create_vcn_details
+    )
     if not vcn:
+        print("Failed to create vcn")
         exit(1)
 
-    stack = {"vcn": vcn}
+    create_ig_details = oci.core.models.CreateInternetGatewayDetails(
+        compartment_id=compartment_id, is_enabled=True, vcn_id=vcn.id
+    )
+    gateway = create(
+        network_client,
+        "create_internet_gateway",
+        InternetGateway,
+        create_internet_gateway_details=create_ig_details,
+    )
+    if not gateway:
+        print("Failed to create gateway")
+        exit(1)
 
+    # Setup the route table
+    route_rule = prepare_route_rule(
+        gateway.id,
+        cidr_block=None,
+        destination="0.0.0.0/0",
+        destination_type="CIDR_BLOCK",
+    )
+
+    route_rules = []
+    if route_rule:
+        route_rules.append(route_rule)
+
+    create_rt_details = CreateRouteTableDetails(
+        compartment_id=compartment_id, route_rules=route_rules, vcn_id=vcn.id
+    )
+    route_table = create(
+        network_client,
+        "create_route_table",
+        RouteTable,
+        create_route_table_details=create_rt_details,
+    )
+
+    # Create subnet
+    create_subnet_details = CreateSubnetDetails(
+        compartment_id=compartment_id, cidr_block=subnet_cidr_block, vcn_id=vcn.id
+    )
+    subnet = create(
+        network_client,
+        "create_subnet",
+        Subnet,
+        create_subnet_details=create_subnet_details,
+    )
+
+    stack = {"id": vcn.id, "worker_subnets": [], "lb_subnets": []}
     return stack
 
 
@@ -31,30 +100,107 @@ def delete_vcn_stack(network_client, compartment_id, name=None, vcn_id=None):
     if not name and not vcn_id:
         return False
 
+    if vcn_id:
+        vcn = get(network_client, "get_vcn", vcn_id)
+    else:
+        # Find vcns with the name
+        vcns = list_entities(
+            network_client, "list_vcns", compartment_id, display_name=name
+        )
+        vcn = vcns[0]
+
     removed_stack = {}
-
-    vcn = get_vcn(
-        network_client, vcn_id=vcn_id, name=name, compartment_id=compartment_id
-    )
     if vcn:
-        deleted_vcn = delete_vcn(network_client, vcn.id)
-        removed_stack.update({"vcn": deleted_vcn})
+        vcn_subnets = list_entities(
+            network_client, "list_subnets", compartment_id, vcn.id
+        )
+        for subnet in vcn_subnets:
+            deleted = delete(network_client, "delete_subnet", Vcn, subnet.id)
 
+        # Delete all the routes (and disable the gateway target if they are the default which
+        # means that they can't be deleted)
+        routes = list_entities(
+            network_client, "list_route_tables", compartment_id, vcn.id
+        )
+        for route in routes:
+            update_details = UpdateRouteTableDetails(route_rules=[])
+            update(
+                network_client,
+                "update_route_table",
+                RouteTable,
+                route.id,
+                update_route_table_details=update_details,
+            )
+
+            delete(network_client, "delete_route_table", RouteTable, route.id)
+
+        # Delete all gateways
+        gateways = list_entities(
+            network_client, "list_internet_gateways", compartment_id, vcn.id
+        )
+        for gateway in gateways:
+            delete(
+                network_client, "delete_internet_gateway", InternetGateway, gateway.id
+            )
+
+        # Delete all security lists
+        securities = list_entities(
+            network_client, "list_security_lists", compartment_id, vcn.id
+        )
+        for security in securities:
+            delete(network_client, "delete_security_list", SecurityList, security.id)
+
+        # Delete all DHCP options
+        dhcp_options = list_entities(
+            network_client, "list_dhcp_options", compartment_id, vcn.id
+        )
+        for dhcp_option in dhcp_options:
+            delete(network_client, "delete_dhcp_options", DhcpOptions, dhcp_option.id)
+
+        # Delete local peering gateways
+        local_peering_gateways = list_entities(
+            network_client, "list_local_peering_gateways", compartment_id, vcn.id
+        )
+        for local_peering_gateway in local_peering_gateways:
+            delete(
+                network_client,
+                "delete_local_peering_gateway",
+                LocalPeeringGateway,
+                local_peering_gateway.id,
+            )
+
+        # Delete all NAT gateways
+        nat_gateways = list_entities(
+            network_client, "list_nat_gateways", compartment_id, vcn_id=vcn.id
+        )
+        for gateway in nat_gateways:
+            delete(network_client, "delete_nat_gateway", NatGateway, gateway.id)
+
+        # Delete Service Gateways
+        service_gateways = list_entities(
+            network_client, "list_service_gateways", compartment_id, vcn_id=vcn.id
+        )
+        for service_gateway in service_gateways:
+            delete(
+                network_client,
+                "delete_service_gateway",
+                ServiceGateway,
+                service_gateway.id,
+            )
+
+        error = delete(network_client, "delete_vcn", Vcn, vcn.id)
+        if not error:
+            removed_stack.update({"vcn": vcn.id})
     # TODO create IG and Subnet
-
     return removed_stack
 
 
 def delete_compartment_vcns(network_client, compartment_id):
-
     removed_vcns = []
-    vcns = list_vcn(network_client, compartment_id)
+    vcns = list_entities(network_client, "list_vcns", compartment_id)
     for vcn in vcns:
-        # TODO, delete all subnets first
-
-        deleted_vcn = delete_vcn(network_client, vcn.id)
-        removed_vcns.append(delete_vcn)
-
+        result = delete_vcn_stack(network_client, compartment_id, vcn_id=vcn.id)
+        removed_vcns.append(result)
     return removed_vcns
 
 
@@ -70,7 +216,8 @@ if __name__ == "__main__":
         network_client,
         args.compartment_id,
         name=args.vcn_name,
-        cidr_block=args.cidr_block,
+        vcn_cidr_block=args.vcn_cidr_block,
+        subnet_cidr_block=args.vcn_subnet_cidr_block,
     )
     print("Create stack: {}".format(stack))
 
