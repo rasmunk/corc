528129fa945543e48c98ffbcb4d1d3e57e1f3c9f
Rasmus Munk <munk1@live.dk>
2020-05-06 20:29:30 +0200

    Start of testing

---
+++ tests/test_cluster_orchestrator.py
@@ -0,0 +1,20 @@
+import pytest
+import unittest
+from orchestration.cluster import OCIClusterOrchestrator
+
+
+class TestClusterOrchestrator(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_cluster_orchestrator(self):
+        pass
+
+    def test_init_cluster(self):
+        pass
+
+    def test_schedule_job_on_cluster(self):
+        pass
