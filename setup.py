7f38231388572ec07b99a7b8330ce24e03ef95cd
Rasmus Munk <munk1@live.dk>
2020-05-06 20:30:34 +0200

    Reformat setup and version

--- setup.py
+++ setup.py
@@ -7,31 +7,30 @@ from setuptools import setup, find_packages
 cur_dir = os.path.abspath(os.path.dirname(__file__))
 
 version_ns = {}
-with open(os.path.join(cur_dir, 'version.py')) as f:
+with open(os.path.join(cur_dir, "version.py")) as f:
     exec(f.read(), {}, version_ns)
 
-long_description = open('README.rst').read()
+long_description = open("README.rst").read()
 setup(
-    name='oci_orchestration',
-    version=version_ns['__version__'],
-    description="A set of helper functions to orchestrate Oracle Cloud Infrastructure entities",
+    name="oci_orchestration",
+    version=version_ns["__version__"],
+    description="A set of helper functions to "
+    "orchestrate Oracle Cloud Infrastructure entities",
     long_description=long_description,
     author="Rasmus Munk",
     author_email="munk1@live.dk",
     packages=find_packages(),
     url="https://github.com/rasmunk/oci_orchestration",
     license="MIT",
-    keywords=['OCI', 'Orchestration'],
-    install_requires=[
-        'oci',
-    ],
+    keywords=["OCI", "Orchestration"],
+    install_requires=["ansible", "kubernetes", "oci"],
     classifiers=[
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 )
--- version.py
+++ version.py
@@ -1,10 +1,6 @@
-version_info = (
-    0,
-    0,
-    0
-)
+version_info = (0, 0, 0)
 
-__version__ = '.'.join(map(str, version_info[:3]))
+__version__ = ".".join(map(str, version_info[:3]))
 
 if len(version_info) > 3:
-    __version__ = '%s-%s' % (__version__, version_info[3])
+    __version__ = "%s-%s" % (__version__, version_info[3])
