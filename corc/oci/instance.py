e8497416d598e39ed6dd527142f1eef5d182b4fc
Rasmus Munk <munk1@live.dk>
2020-05-25 09:09:23 +0200

    Update instance module to rely on common oci methods

--- corc/oci/instance.py
+++ corc/oci/instance.py
@@ -1,38 +1,25 @@
 import oci
 from corc.helpers import create_internet_gateway, get_subnet_gateway_id, new_client
 from corc.cli.args import get_arguments, OCI, COMPUTE
+from corc.oci.helpers import create, delete
 
 
-def stop():
-    args = get_arguments([OCI, COMPUTE], strip_group_prefix=True)
-    config = oci.config.from_file(profile_name=args.profile_name)
-    oci.config.validate_config(config)
-    compute_client = new_client(oci.core.ComputeClient, profile_name=args.profile_name)
-
-    instances = get_instances(
+def launch_instance(compute_client, launch_instance_details):
+    instance = create(
         compute_client,
-        args.compartment_id,
-        lifecycle_state=[
+        'launch_instance',
+        wait_for_states=[
             oci.core.models.Instance.LIFECYCLE_STATE_PROVISIONING,
             oci.core.models.Instance.LIFECYCLE_STATE_STARTING,
-            oci.core.models.Instance.LIFECYCLE_STATE_RUNNING,
+            oci.core.models.Instance.LIFECYCLE_STATE_RUNNING
         ],
+        launch_instance_details=launch_instance_details,
     )
+    return instance
 
-    terminate_options = dict(preserve_boot_volume=False,)
 
-    # Stop or terminate all instances
-    for instance in instances:
-        compute_client.terminate_instance(instance.id, **terminate_options)
-
-    # Verify it is terminating
-    terminate_instances = compute_client.list_instances(args.compartment_id).data
-    terminated_states = [
-        oci.core.models.Instance.LIFECYCLE_STATE_TERMINATING,
-        oci.core.models.Instance.LIFECYCLE_STATE_TERMINATED,
-    ]
-    for instance in terminate_instances:
-        assert instance.lifecycle_state in terminated_states
+def terminate_instance(compute_client, instance_id, **kwargs):
+    return delete(compute_client, 'terminate_instance', instance_id, **kwargs)
 
 
 if __name__ == "__main__":
