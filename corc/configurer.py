5bc4f8c723947bcbf8fd1b517892e709b6ec916a
Rasmus Munk <munk1@live.dk>
2020-05-27 21:48:59 +0200

    Add the initial structure for introducing the configurer

---
+++ corc/configurer.py
@@ -0,0 +1,20 @@
+from ansible.parsing.dataloader import DataLoader
+from ansible.inventory.manager import InventoryManager
+from ansible.vars.manager import VariableManager
+from ansible.playbook import Playbook
+from ansible.executor.task_queue_manager import TaskQueueManager
+
+
+def load_playbook_source(path):
+    loader = DataLoader()
+    play_source = loader.load_from_file(path)
+    return play_source
+
+
+class AnsibleConfigurer
+
+    def __init__(self, options):
+        self.options = options
+        self.loader = DataLoader()
+    
+    
---
+++ tests/test_instance_configurer.py
@@ -0,0 +1,60 @@
+import unittest
+from corc.configurer import AnsibleConfigurer
+from corc.oci.instance import OCIInstanceOrchestrator
+
+
+class TestInstanceConfigurer(unittest.TestCase):
+    def setUp(self):
+        oci_options = dict(
+            compartment_id="ocid1.tenancy.oc1..aaaaaaaakfmksyrf7hl2gfexmjpb6pbyrirm6k3ro7wd464y2pr7atpxpv4q",
+            profile_name="XNOVOTECH",
+        )
+
+        compute_options = dict(
+            availability_domain="Xfze:eu-amsterdam-1-AD-1",
+            shape="VM.Standard2.1",
+            operating_system="CentOS",
+            operating_system_version="7",
+            display_name="Test Node"
+        )
+
+        compute_metadata_options = dict(
+            ssh_authorized_keys=[
+                "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpRktqNSSLq1ARcMAuuTq3I8/K3CgcPJ3CVlXfU2mxg1zSrIwFOEb+foW2jUqEFcwdCmY/gI+XxBaJHxQLIqzowl0C4d6FVtbnRCfNShSbWr4p7xY0FDJvDMD7B7f7XT8zQoCX7Qnugo/afTPxz1R8mAfLFKU97Cy5zr3Bh8mW/ipgKNfH573k50Qe9CN/S9GjtGB2bGPZGSIFpQ6tfmkssBQIkmym7UxfNgQfeV/1drc02GTqH850d7jIXsMCO8XpxQaeVl/G+1/wwAxv+Nna2s143wH6MmAzrklRyb1jQ+ip/fhVF+l4Kk8a2E+DmWsBWj5vmpRLL7hS2MHPszkp"
+            ]
+        )
+
+        vcn_options = dict(
+            cidr_block="10.0.0.0/16",
+            display_name="Test Instance Network",
+            dns_label="xnovotech",
+        )
+        subnet_options = dict(display_name="Test Instance Subnet", dns_label="workers")
+
+        self.options = dict(
+            oci=oci_options,
+            compute=compute_options,
+            compute_metadata=compute_metadata_options,
+            vcn=vcn_options,
+            subnet=subnet_options,
+        )
+
+        OCIInstanceOrchestrator.validate_options(self.options)
+        self.orchestrator = OCIInstanceOrchestrator(self.options)
+        # Should not be ready at this point
+        self.orchestrator.setup()
+        self.assertTrue(self.orchestrator.is_ready())
+
+
+    def tearDown(self):
+        self.orchestrator.tear_down()
+        self.assertFalse(self.orchestrator.is_ready())
+        self.orchestrator = None
+        self.options = None
+
+    def test_instance_ansible_configure(self):
+        configurer = AnsibleConfigurer()
+
+
+if __name__ == "__main__":
+    unittest.main()
