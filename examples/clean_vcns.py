4d99ab968ab146d1db89e1b6443c45381c617779
Rasmus Munk <munk1@live.dk>
2020-06-05 15:39:03 +0200

    Add helper script to delete every vcn inside a comparment

---
+++ examples/clean_vcns.py
@@ -0,0 +1,14 @@
+from oci.core import VirtualNetworkClient, VirtualNetworkClientCompositeOperations
+from corc.oci.helpers import new_client
+from corc.oci.network import delete_compartment_vcns
+
+
+if __name__ == "__main__":
+    compartment_id = ""
+    network_client = new_client(
+        VirtualNetworkClient,
+        composite_class=VirtualNetworkClientCompositeOperations,
+        profile_name="",
+    )
+
+    delete_compartment_vcns(network_client, compartment_id)
