d0e0bb432d378a983bf6509f8b22439364215b8b
Rasmus Munk <munk1@live.dk>
2020-11-15 16:30:53 +0100

    Fix too long line [ci]

--- corc/providers/apache/config_ec2.py
+++ corc/providers/apache/config_ec2.py
@@ -57,7 +57,9 @@ def load_driver_options(
             config_path = os.path.expanduser(provider_kwargs["profile"]["config_file"])
 
         if "credentials_file" in provider_kwargs["profile"]:
-            credentials_path = os.path.expanduser(provider_kwargs["profile"]["credentials_file"])
+            credentials_path = os.path.expanduser(
+                provider_kwargs["profile"]["credentials_file"]
+            )
 
         if "aws_access_key_id" in provider_kwargs["profile"]:
             aws_access_key_id = provider_kwargs["profile"]["aws_access_key_id"]
