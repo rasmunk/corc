d13d1c4e04dce042ff833fac881c0b2b14135a77
Rasmus Munk <munk1@live.dk>
2023-01-20 10:53:31 +0100

    Rename aws_group to ec2_group

--- corc/cli/parsers/providers/ec2/profile.py
+++ corc/cli/parsers/providers/ec2/profile.py
@@ -3,9 +3,9 @@ def valid_ec2_group(parser):
 
 
 def profile_group(parser):
-    aws_group = parser.add_argument_group(title="EC2 arguments")
-    aws_group.add_argument("--ec2-profile-name", default="")
-    aws_group.add_argument("--ec2-profile-region-name", default="")
+    ec2_group = parser.add_argument_group(title="EC2 arguments")
+    ec2_group.add_argument("--ec2-profile-name", default="")
+    ec2_group.add_argument("--ec2-profile-region-name", default="")
 
     # HACK to extract the set provider from the cli
-    aws_group.add_argument("--ec2", action="store_true", default=True)
+    ec2_group.add_argument("--ec2", action="store_true", default=True)
