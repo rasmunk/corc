eeab702778ad94841a892699f4ae46cf0b0d211e
Rasmus Munk <munk1@live.dk>
2023-01-21 11:29:22 +0100

    Remove duplicant configurer parser

--- corc/cli/configurer/ansible.py
+++
@@ -1,9 +0,0 @@
-def valid_ansible_group(parser):
-    add_ansible_group(parser)
-
-
-def add_ansible_group(parser):
-    ansible_group = parser.add_argument_group(title="Ansible arguments")
-    ansible_group.add_argument("--ansible-root-path", default=False)
-    ansible_group.add_argument("--ansible-playbook-path", default=False)
-    ansible_group.add_argument("--ansible-inventory-path", default=False)
