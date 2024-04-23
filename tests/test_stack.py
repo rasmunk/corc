a7cb88ec41adf40bd33018e54221427371246e27
Rasmus Munk <munk1@live.dk>
2024-04-23 13:45:43 +0200

    Add stack tests

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,20 +1,69 @@
 import unittest
-from corc.core.orchestration.stack.models import Stack
-from corc.core.orchestration.pool.models import Pool, Instance
-from corc.core.orchestration.pool.add_instance import add_instance
-from corc.core.orchestration.pool.remove_instance import remove_instance
+import uuid
+import copy
+import subprocess
+import os
+from corc.core.defaults import STACK
+from corc.utils.io import join
+from corc.core.storage.dictdatabase import DictDatabase
 
 
 class TestStack(unittest.IsolatedAsyncioTestCase):
     async def asyncSetUp(self):
         self.name = "dummy"
+        self.base_args = ["corc", "orchestration", STACK]
 
     async def asyncTearDown(self):
         # Ensure that any pool is destroyed
-        pool = Pool(self.name)
-        for node in await pool.items():
-            removed, response = await remove_instance(self.name, node.id)
-            self.assertTrue(removed)
-        self.assertTrue(await pool.flush())
-        self.assertEqual(len(await pool.items()), 0)
-        self.assertTrue(await pool.remove_persistence())
+        stack_db = DictDatabase(STACK)
+        self.assertTrue(await stack_db.flush())
+        self.assertEqual(len(await stack_db.items()), 0)
+        self.assertTrue(await stack_db.remove_persistence())
+        self.assertFalse(await stack_db.exists())
+
+    async def test_dummy_stack_deploy(self):
+        test_id = str(uuid.uuid4())
+        name = f"{self.name}-{test_id}"
+        deploy_file = join("tests", "res", "test-stack.yml")
+        create_stack_args = copy.deepcopy(self.base_args)
+        create_stack_args.extend(["deploy", name, deploy_file])
+        result = subprocess.run(create_stack_args)
+        self.assertIsNotNone(result)
+        self.assertTrue(hasattr(result, "returncode"))
+        self.assertEqual(result.returncode, 0)
+
+        # Check that the stack exists
+        stack_db = DictDatabase(STACK)
+        self.assertTrue(await stack_db.exists())
+        stack = await stack_db.get(name)
+        self.assertIsNotNone(stack)
+        self.assertEqual(stack["name"], name)
+
+        # Remove and validate that it is gone
+        self.assertTrue(await stack_db.flush())
+        self.assertTrue(await stack_db.remove_persistence())
+        self.assertFalse(await stack_db.exists())
+
+    async def test_dummy_stack_destroy(self):
+        # Create a stack that can be removed by the CLI
+        test_id = str(uuid.uuid4())
+        name = f"{self.name}-{test_id}"
+        stack_db = DictDatabase(STACK)
+        self.assertTrue(stack_db.touch())
+
+        # Create stack instance to be removed
+        deploy_file = join("tests", "res", "test-stack.yml")
+        create_stack_args = copy.deepcopy(self.base_args)
+        create_stack_args.extend(["deploy", name, deploy_file])
+
+        result = subprocess.run(create_stack_args)
+        self.assertIsNotNone(result)
+        self.assertTrue(hasattr(result, "returncode"))
+        self.assertEqual(result.returncode, 0)
+
+        remove_stack_args = copy.deepcopy(self.base_args)
+        remove_stack_args.extend(["destroy", name])
+        result = subprocess.run(remove_stack_args)
+        self.assertIsNotNone(result)
+        self.assertTrue(hasattr(result, "returncode"))
+        self.assertEqual(result.returncode, 0)
