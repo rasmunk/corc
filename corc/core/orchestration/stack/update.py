9516c3f6f6fafb6177cd60f781eb9cd4786e9a1d
Rasmus Munk <rasmunk@users.noreply.github.com>
2024-11-03 20:51:41 +0100

    Align the stack update call to use the same return response structure

--- corc/core/orchestration/stack/update.py
+++ corc/core/orchestration/stack/update.py
@@ -14,7 +14,11 @@ async def update(*args, **kwargs):
 
     stack_db = DictDatabase(name, directory=directory)
     if not await stack_db.exists():
-        response["msg"] = "The Stack: {} database does not exist.".format(name)
+        response["msg"] = (
+            "The Stack: {} database does not exist in directory: {}.".format(
+                name, directory
+            )
+        )
         return False, response
 
     stack_to_update = await stack_db.get(name)
@@ -29,13 +33,15 @@ async def update(*args, **kwargs):
     # Load the architecture file and deploy the stack
     stack_config = await get_stack_config(config_file)
     if not stack_config:
-        return False, {
-            "error": "Failed to load the Stack configuration file: {}.".format(
-                config_file
-            )
-        }
+        response["msg"] = "Failed to load the Stack configuration file: {}.".format(
+            config_file
+        )
+        return False, response
+
+    # Extract the pool configurations
     stack_to_update["config"]["pools"] = await get_stack_config_pools(stack_config)
 
+    # Extract the instance configurations
     instances_success, instances_response = await get_stack_config_instances(
         stack_config
     )
@@ -44,11 +50,12 @@ async def update(*args, **kwargs):
     stack_to_update["config"]["instances"] = instances_response
 
     if not await stack_db.update(name, stack_to_update):
-        return False, {
-            "error": "Failed to save the updated Stack information to the database: {}".format(
+        response["msg"] = (
+            "Failed to save the updated Stack information to the database: {}".format(
                 stack_db.name
             )
-        }
+        )
+        return False, response
 
     response["msg"] = "The Stack: {} has been updated.".format(name)
     return True, response
