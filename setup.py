8249fdd5a3858c5346bab8441952e9d8e7f4740a
Rasmus Munk <rasmunk@users.noreply.github.com>
2025-10-23 10:07:38 +0200

    Remove the python 3.8 classifier and add the 3.13

--- setup.py
+++ setup.py
@@ -62,10 +62,10 @@ setup(
         "Intended Audience :: System Administrators",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
 )
