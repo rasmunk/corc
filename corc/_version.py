2f8abb49132d17eb234f2030f8587116377b259b
Rasmus Munk <rasmunk@users.noreply.github.com>
2026-02-17 14:05:04 +0100

    Release version 0.0.3a2
    This release changes the license from MIT to GPLv2. It also does alot of project restructuring such as moving the PLAN construct to be owned by a STACK. Furthermore it adds additional tests to cover stacks and pools

--- corc/_version.py
+++ corc/_version.py
@@ -14,7 +14,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-version_info = (0, 0, 3, "a1")
+version_info = (0, 0, 3, "a2")
 
 __version__ = ".".join(map(str, version_info[:3]))
 
